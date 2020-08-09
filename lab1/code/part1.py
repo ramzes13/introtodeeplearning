2040238d6058c246a5c294c061fbcea951a03270
darii petru <darii.petru@gmail.com>
2020-08-09 11:18:55 +0000

    finish lab 1

--- lab1/code/part1.py
+++ lab1/code/part1.py
@@ -19,3 +19,34 @@ numbers = tf.constant([3.141592, 1.414213, 2.71821], tf.float64)
 
 print("`sports` is a {}-d Tensor with shape: {}".format(tf.rank(sports).numpy(), tf.shape(sports)))
 print("`numbers` is a {}-d Tensor with shape: {}".format(tf.rank(numbers).numpy(), tf.shape(numbers)))
+
+
+# ### Defining higher-order Tensors ###
+
+# '''TODO: Define a 2-d Tensor'''
+matrix = tf.constant([[1.0, 2.0, 3.0, 4.0], [5.0, 6.0, 7.0, 8.0]])
+
+print("`matrix` is a {}-d Tensor with shape: {}".format(tf.rank(matrix).numpy(), tf.shape(matrix)))
+
+assert isinstance(matrix, tf.Tensor), "matrix must be a tf Tensor object"
+assert tf.rank(matrix).numpy() == 2
+
+'''TODO: Define a 4-d Tensor.'''
+# Use tf.zeros to initialize a 4-d Tensor of zeros with size 10 x 256 x 256 x 3.
+#   You can think of this as 10 images where each image is RGB 256 x 256.
+images = tf.zeros([10, 256, 256, 3], tf.int32)
+
+print("`images` is a {}-d Tensor with shape: {}".format(tf.rank(images).numpy(), tf.shape(images)))
+
+assert isinstance(images, tf.Tensor), "matrix must be a tf Tensor object"
+assert tf.rank(images).numpy() == 4, "matrix must be of rank 4"
+assert tf.shape(images).numpy().tolist() == [
+    10, 256, 256, 3], "matrix is incorrect shape"
+
+row_vector = matrix[1]
+column_vector = matrix[:,2]
+scalar = matrix[1, 2]
+
+print("`row_vector`: {}".format(row_vector.numpy()))
+print("`column_vector`: {}".format(column_vector.numpy()))
+print("`scalar`: {}".format(scalar.numpy()))
